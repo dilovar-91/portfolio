f3cb23517cb29572e37f0c786a3b8d4c9fd98a37
Dilovar Azimov <dilovar09@gmail.com>
2020-12-04 14:48:47 +0300

    exec_mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'azimoff-me',
       port: 3200,
-      exec_mode: 'cluster',
+      // exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
