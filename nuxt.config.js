431a789f894d6dd82ec86fecb7bd12e0f01425ab
Dilovar Azimov <dilovar09@gmail.com>
2021-01-16 18:04:18 +0300

    treeShake: true

--- nuxt.config.js
+++ nuxt.config.js
@@ -112,9 +112,7 @@ export default {
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
-    loader: {
-      registerStylesSSR: true
-    },
+    treeShake: true,
     customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: false,
